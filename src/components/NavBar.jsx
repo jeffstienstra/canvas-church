590239c7cb84c00ec68e88b4968f3565116a203a
jeff-stienstra <jeff.stienstra@envoyb2b.com>
2022-12-15 10:08:49 -0500

    add header with basic mobile-friendly nav setup

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
 import Logo from '../assets/canvas-church-logo-transparent.svg'
 
 const NavBar = () => {
+    const [showNav, setShowNav] = useState(false);
+    const handleClick = () => setShowNav(!showNav);
     return (
-        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#181d20] text-gray-300' >
-            <div>
-                <img src={Logo} alt="Canvas Church Logo" className='h-[60px]' />
+        <header className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#181d20] text-gray-300' >
+            <div className='flex w-[128px] mx-4'>
+                <a href="/" >
+                    <img src={Logo} alt="Canvas Church Logo"
+                        className='w-[116px] hover:w-[128px] hover:mx-[-3px] duration-200'
+                    />
+                </a>
             </div>
-        </div>
+            {/* menu */}
+            <ul className='hidden md:flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+
+            {/* hamburger */}
+            <div onClick={handleClick} className='md:hidden z-10 cursor-pointer'>
+                {!showNav ? <FaBars /> : <FaTimes  />}
+            </div>
+
+            {/* mobile menu */}
+            <ul className={!showNav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#181d20] flex flex-col justify-center items-center'}>
+                <li className='py-6 text-4xl'>Home</li>
+                <li className='py-6 text-4xl'>About</li>
+                <li className='py-6 text-4xl'>Skills</li>
+                <li className='py-6 text-4xl'>Work</li>
+                <li className='py-6 text-4xl'>Contact</li>
+            </ul>
+
+            {/* social icons */}
+            <div className='hidden'>
+            </div>
+        </header>
     )
 }
 
