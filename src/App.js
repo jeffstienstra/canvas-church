6d1fadc44806e6f6d543b399b95872cbb9a254d3
jeff-stienstra <jeff.stienstra@envoyb2b.com>
2022-12-16 09:32:04 -0500

    implement react-router-dom v6+

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import NavBar from './components/NavBar'
 import Home from './components/Home';
 import About from './components/About';
+import Community from './components/Community';
 import MainNav from './components/MainNav';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <>
       <MainNav />
-      {/* <NavBar /> */}
-      <Home />
-      <About />
-    </div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/community' element={<Community/>}/>
+        {/* <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/> */}
+      </Routes>
+    </>
   );
 }
 
